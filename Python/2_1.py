3cc31d8978d10ef64bb49f75aba74661ef1485da
“RzMY” <sophia63457@gmail.com>
2023-03-12 12:22:46 +0800

    2_1 2_2

---
+++ Python/2_1.py
@@ -0,0 +1,3 @@
+#将一条消息存储到变量中，再将其打印出来
+message="hello world!"
+print(message)
---
+++ Python/2_2.py
@@ -0,0 +1,5 @@
+#将一条消息存储到变量中，再将其打印出来；再将变量的值修改为一条新消息，再将其打印出来
+message="hello world!"
+print(message)
+message="hello python!"
+print(message)
