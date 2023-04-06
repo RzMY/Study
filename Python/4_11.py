5703c422a58eb70e3fe4f0981a99bb125b576f63
RzMY <sophia63475@gmail.com>
2023-04-06 16:09:21 +0800

    Python

--- Python/4_11.py
+++ Python/4_11.py
@@ -1,7 +1,15 @@
 pizza = ['pepperoni', 'sausage', 'cheese', 'mushroom', 'pineapple']
-for i in pizza:
-    message = f"I like {i} pizza."
+for pizza_0 in pizza:
+    message = f"I like {pizza_0} pizza."
     print(message)
 print("I really love pizza!")
 friend_pizzas = pizza[:]#复制列表
 #friend_pizzas = pizza#直接赋值，friend_pizzas和pizza指向同一个列表
+pizza.append('bacon')
+friend_pizzas.append('chicken') 
+print(f"My favorite pizzas are:")
+for pizza_1 in pizza:
+    print(pizza_1)
+print(f"My friend's favorite pizzas are:")
+for pizza_2 in friend_pizzas:
+    print(pizza_2)
---
+++ Python/4_13.py
@@ -0,0 +1,7 @@
+buffet_food = ('pizza', 'falafel', 'carrot cake', 'ice cream', 'cannoli')
+for buffet_food_0 in buffet_food:
+    print(buffet_food_0)
+#buffet_food[0] = 'chicken' #Python禁止修改元组的元素
+buffet_food_new = ('chicken', 'falafel', 'carrot cake', 'ice cream', 'cannoli')
+for buffet_food_1 in buffet_food_new:
+    print(buffet_food_1)
