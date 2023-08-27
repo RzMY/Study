55a311f17c5f1338112bc048e45fa8254b84c59b
RzMY <sophia63475@gmail.com>
2023-08-27 17:37:17 +0800

    python

--- Leetcode/48.旋转图像.py
+++ Leetcode/48.旋转图像.py
@@ -9,6 +9,23 @@ class Solution:
     def rotate(self, matrix: list[list[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
-        """   
+        """
+        matrix.reverse()
+        
+        i = 0
+        while i < len(matrix):
+            j = 0
+            while j < len(matrix):
+                matrix[i].append(matrix[j][i])
+                j += 1
+            i += 1
+                
+        i = len(matrix) - 1
+        while i >= 0:
+            j = len(matrix) - 1
+            while j >= 0:
+                del matrix[j][0]
+                j -= 1
+            i -= 1
 # @lc code=end
 
