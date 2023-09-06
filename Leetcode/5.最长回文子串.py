9cad1fe502482ebd23a88b9e19be0d5df1f47a1c
RzMY <sophia63475@gmail.com>
2023-09-06 15:48:26 +0800

    Leetcode

---
+++ Leetcode/5.最长回文子串.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=5 lang=python3
+#
+# [5] 最长回文子串
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        # 时间复杂度过高（字符串反转操作为O(n)）
+        
+        # for i in reversed(range(len(s) + 1)):
+        #     for j in range(len(s)):
+        #         if s[j:i+j][::-1] == s[j:i+j] and i + j <= len(s):
+        #             return s[j:i+j]
+        
+        # 从待判断的字符串中间开始向外比较
+        
+        for i in reversed(range(len(s) + 1)):
+            for j in range(len(s)):
+                if i + j <= len(s):
+                    if len(s[j:i+j]) % 2 != 0:
+                        for k in range(len(s[j:i+j])):
+                str = s[j:i+1]
+                if s[j:i+j][::-1] == s[j:i+j] and i + j <= len(s):
+                    return s[j:i+j]
+# @lc code=end
+
